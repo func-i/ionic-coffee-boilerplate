b983fbcf5409797a458ef7a07c77115bc05e5d8d
tfchang <tingfchang@gmail.com>
2015-10-16 15:26:29 -0400

    gulp-usemin does not work

--- gulpfile.js
+++ gulpfile.js
@@ -11,41 +11,52 @@ var runSequence = require('run-sequence');
 gulp.task('clean', function () {
   return del([
     '.tmp/**/*', 
-    // '!.tmp', // ** includes the parent dir
-    'www/**/*', 
-    // '!www'
+    'www/**/*'
   ]);
 });
 
 // inject bower components into the app (web)
 gulp.task('wiredep:web', function () {
-  gulp.src('./app/index.html')
+  return gulp.src('app/index.html')
     .pipe( wiredep({
       devDependencies: true,  // for angular-mocks.js, to mock HTTP response from API
       ignorePath:  /\.\.\//
     }) )
-    .pipe( gulp.dest('./www') );
+    .pipe( gulp.dest('.tmp') );
 });
 
 // coompile CoffeeScript to JavaScript
 // add source maps
 gulp.task('coffee', function() {
-  gulp.src('./app/scripts/**/*.coffee')
+  return gulp.src('app/scripts/**/*.coffee')
     .pipe( plugins.sourcemaps.init() )
     .pipe( plugins.coffee({ bare: true }).on('error', plugins.util.log) )
     .pipe( plugins.sourcemaps.write('.') )
-    .pipe( gulp.dest('./.tmp/js/') )
+    .pipe( gulp.dest('.tmp/js') )
 });
 
 // pre-process SASS to CSS
 // add vendor prefixes and source maps
 gulp.task('sass', function () {
-  gulp.src('./app/styles/**/*.scss')
+  return gulp.src('app/styles/**/*.scss')
     .pipe( plugins.sourcemaps.init() )
     .pipe( plugins.sass().on('error', plugins.sass.logError) )
     .pipe( plugins.autoprefixer({ browsers: ['last 2 versions'] }) )
     .pipe( plugins.sourcemaps.write('.') )
-    .pipe( gulp.dest('./.tmp/css/') );
+    .pipe( gulp.dest('.tmp/css') );
+});
+
+gulp.task('usemin', function() {
+  gulp.src('.tmp/index.html')
+    .pipe( plugins.usemin({
+      // assetsDir: 'www',
+      // css:        [ plugins.rev() ],
+      html:       [ plugins.minifyHtml({ empty: true }) ],
+      js:         [ plugins.uglify() ],
+      inlinejs:   [ plugins.uglify() ],
+      inlinecss:  [ plugins.minifyCss(), 'concat' ]
+    }) )
+    .pipe( gulp.dest('www') );
 });
 
 // connect to local server for development
@@ -73,7 +84,8 @@ gulp.task('serve', function () {
   runSequence(
     'clean',
     ['wiredep:web', 'coffee', 'sass'],
-    'connect', 
-    'watch'
+    'usemin'
+    // 'connect', 
+    // 'watch'
   );
 });
